b10fb54c84ac8f4069db30cc9317497059e75973
Swati Makhija <swatimakhija@Swatis-MacBook-Air.local>
2021-02-25 11:59:27 -0800

    boilerplate code for fruitcontainer component setup

---
+++ src/components/FruitContainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class FruitContainer extends Component {
+    constructor(props) { 
+        super()
+        this.state = {}
+    }
+
+    render () { 
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
+export default FruitContainer; 
